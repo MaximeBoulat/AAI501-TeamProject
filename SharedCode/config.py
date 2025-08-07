2b19fd94802d7800cf9809ac84242d0768c80bb5
dylansd <dylansd@gmail.com>
2025-08-07 09:38:42 -0700

    consistent naming for models and data

--- SharedCode/config.py
+++ SharedCode/config.py
@@ -8,7 +8,7 @@ SEED = 42
 ATTEMPTS = 100
 X_COLS=[f'sensor_{i}' for i in range(8)] + ['distance_to_goal', 'goal_direction']
 Y_HAT_COL=['action']
-NUM_RUNS = 3000
+NUM_RUNS = 10000
 
 # Dynamically Generate Required names based on the above vars 
 size_label = "small"
@@ -29,4 +29,3 @@ def condense(columns: list[str]) -> str:
 
 EXPERIMENT_NAME=f"{Y_HAT_COL[0]}_norm_{size_label}_{condense(X_COLS)}"
 TRAINING_DATA_FILE=f"training_data_{size_label}_{NUM_RUNS // 1_000}k.csv"
-
